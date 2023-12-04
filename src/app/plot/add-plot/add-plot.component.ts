e029fc232d0f0fee45a974e3e18724e8a6c89949
rijantp <rijanriju22@gmail.com>
2023-12-04 17:18:50 +0530

    stepper added

--- src/app/plot/add-plot/add-plot.component.ts
+++ src/app/plot/add-plot/add-plot.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { PlotDetailsComponent } from '../components/plot-details/plot-details.component'
-
+import { MatStepperModule } from '@angular/material/stepper'
 @Component({
   selector: 'app-add-plot',
   standalone: true,
-  imports: [PlotDetailsComponent],
+  imports: [PlotDetailsComponent, MatStepperModule],
   templateUrl: './add-plot.component.html',
   styleUrl: './add-plot.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
