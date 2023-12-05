12de0ceb45801230e13a4e1c661868ff0c2d7b28
rijantp <rijanriju22@gmail.com>
2023-12-05 22:32:45 +0530

    header stylings

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core'
 import { MatSelectModule } from '@angular/material/select'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
+
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSelectModule, MatFormFieldModule],
+  imports: [
+    MatSelectModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
