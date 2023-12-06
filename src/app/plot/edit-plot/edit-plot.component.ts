48a423c46c4bc0fff0d653f04787098da419083c
rijantp <rijanriju22@gmail.com>
2023-12-06 13:05:12 +0530

    edit plot done till status

--- src/app/plot/edit-plot/edit-plot.component.ts
+++ src/app/plot/edit-plot/edit-plot.component.ts
@@ -1,28 +1,43 @@
-import { Component, signal } from '@angular/core'
-import { EditPlotDetailsComponent } from './edit-plot-details/edit-plot-details.component'
-import { EditCropDetailsComponent } from './edit-crop-details/edit-crop-details.component'
-import { EditStandardsComponent } from './edit-standards/edit-standards.component'
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
+import { PlotDetailsComponent } from '../components/plot-details/plot-details.component'
+import { CropDetailsComponent } from '../components/crop-details/crop-details.component'
+import { StandardsComponent } from '../components/standards/standards.component'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-edit-plot',
   standalone: true,
   imports: [
-    EditPlotDetailsComponent,
-    EditCropDetailsComponent,
-    EditStandardsComponent,
+    PlotDetailsComponent,
+    CropDetailsComponent,
+    StandardsComponent,
     CommonModule,
     MatIconModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   templateUrl: './edit-plot.component.html',
   styleUrl: './edit-plot.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditPlotComponent {
   selectedTabSig = signal('Plot details')
+  isSavedSig = signal(false)
   tabs: string[] = ['Plot details', 'Crop details', 'Standards']
 
   onTabSelection(tab: string): void {
     this.selectedTabSig.set(tab)
+  }
+
+  onAutoSave(isValid: boolean): void {
+    if (isValid) {
+      this.isSavedSig.set(isValid)
+      setTimeout(() => {
+        this.isSavedSig.set(false)
+      }, 3000)
+    }
   }
 }
