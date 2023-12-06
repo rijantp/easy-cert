ac7249e032cbf3727618938945076f235359f228
rijantp <rijanriju22@gmail.com>
2023-12-06 17:42:25 +0530

    language dropdown

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,8 @@ import { MatSelectModule } from '@angular/material/select'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatMenuModule } from '@angular/material/menu'
 import { MatButtonModule } from '@angular/material/button'
-
 import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-header',
@@ -15,8 +15,11 @@ import { MatIconModule } from '@angular/material/icon'
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
+    FormsModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  langSelected: string = 'Eng'
+}
